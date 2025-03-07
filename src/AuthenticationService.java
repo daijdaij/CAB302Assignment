3566bd5a4386f944c8ac5d6cd5a46348c854fb1f
Thompson <thompsontang13@gmail.com>
2025-03-08 09:46:55 +1000

    Implemented the signUp method

--- src/AuthenticationService.java
+++ src/AuthenticationService.java
@@ -1,17 +1,31 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class AuthenticationService implements IAuthenticationService {
     private ArrayList<User> users;
 
-    // TODO Now: Implement the signUp method to add a new user to the list if the username is not taken and return the user; returns null otherwise
+    // Constructor to initialize the users list with the default user
+    public AuthenticationService() {
+        users.add(new User("test", "test"));
+    }
+
+    // signUp method to add a new user to the list if the username is not taken and return the user; returns null otherwise
     @Override
     public User signUp(String username, String password) {
-        return null;
+        for (User existAlready : users) {
+            if (existAlready.getUsername().equals(username)) {
+                return null;
+            }
+        }
+        User newUser = new User(username, password);
+        users.add(newUser);
+        return newUser;
     }
 
     // TODO Now: Implement the logIn method to return the user if the username and password match, and null otherwise
     @Override
     public User logIn(String username, String password) {
+
         return null;
     }
 }
