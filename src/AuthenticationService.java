b1f2a989c7517c5334090ab1605f8ad208fbace9
Thompson <thompsontang13@gmail.com>
2025-03-07 10:07:08 +1000

    Re-adding in the needed class

---
+++ src/AuthenticationService.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class AuthenticationService implements IAuthenticationService {
+    private ArrayList<User> users;
+
+    // TODO Now: Implement the signUp method to add a new user to the list if the username is not taken and return the user; returns null otherwise
+    @Override
+    public User signUp(String username, String password) {
+        return null;
+    }
+
+    // TODO Now: Implement the logIn method to return the user if the username and password match, and null otherwise
+    @Override
+    public User logIn(String username, String password) {
+        return null;
+    }
+}
+
